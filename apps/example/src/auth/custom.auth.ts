c6fd6d00b0bc24b6c4a7ca3ff24d749821b0a3c3
Aníbal Jorquera <ajorquera.cornejo@gmail.com>
2025-11-07 14:49:09 -0300

    refactor: change lambda asset creator

---
+++ apps/example/src/auth/custom.auth.ts
@@ -0,0 +1,20 @@
+import {
+  type AuthorizationHandlerEvent,
+  AuthorizerHandler,
+  type AuthorizerResponse,
+  CustomAuthorizer,
+} from '@alicanto/api/main';
+
+@CustomAuthorizer({
+  name: 'ExampleCustomAuthorizer',
+  header: 'Authorization',
+})
+export class ExampleCustomAuthorizer {
+  @AuthorizerHandler()
+  handler(_e: AuthorizationHandlerEvent): AuthorizerResponse {
+    return {
+      allow: true,
+      principalId: 'example@example.com',
+    };
+  }
+}
