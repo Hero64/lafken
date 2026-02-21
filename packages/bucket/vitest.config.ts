43fd4743b63424594e331b2848a7c1ae8f718237
Aníbal Jorquera <ajorquera.cornejo@gmail.com>
2026-02-21 17:04:45 -0300

    refactor(bucket): add path aliasing for module resolution

--- packages/bucket/vitest.config.ts
+++ packages/bucket/vitest.config.ts
@@ -1,8 +1,17 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     watch: false,
     setupFiles: ['./vitest.setup.ts'],
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^@lafken\/(.*)$/,
+        replacement: path.resolve(__dirname, '../$1/src'),
+      },
+    ],
   },
 });
